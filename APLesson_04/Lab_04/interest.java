4360dbdcaffd2ca6848c66b27e046e68c0546a5f
Rohith Kodukula <kodukula.rohith@gmail.com>
2016-09-22 11:17:29 -0700

    finished lesson 4

---
+++ APLesson_04/Lab_04/interest.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class interest
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		
+		interest calc = new interest();
+		
+		System.out.println("What is the principle amount of money?");
+		double p = kb.nextDouble();
+		
+		System.out.println("What is the interest rate?");
+		double r = kb.nextDouble();
+		
+		System.out.println("How much time is it going to be compounded for?");
+		double t = kb.nextDouble();
+		
+		System.out.println("How many times per year is it interest calculated?");
+		double n = kb.nextDouble();
+		
+		double intr = calc.calc(r,p, n,t);
+		System.out.println("Your total payment is $" + intr);
+	}
+	public double calc(double r, double p, double n, double t)
+	{
+		return p*(Math.pow((1+(r/n)),n*t));
+	}
+}
